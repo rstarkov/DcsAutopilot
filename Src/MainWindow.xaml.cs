629a57305459af1abe1c119c2450c78be8ef3c38
Roman Starkov <sourcecode@roman.st>
2023-05-06 16:43:30 +0100

    Empty WPF project

---
+++ Src/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace DcsAutopilot;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
