f109d2042d8014926f9ce902b60259530cddb2bd
Roman Starkov <sourcecode@roman.st>
2023-05-08 18:06:34 +0100

    Limit integral windup by the magnitude of the derivative. Tuning tweaks.

--- Src/Controllers.cs
+++ Src/Controllers.cs
@@ -1,11 +1,12 @@
+using System;
 using RT.Util.ExtensionMethods;
 
 namespace DcsAutopilot;
 
 class BasicAltitudeController : IFlightController
 {
-    private BasicPid _vspeed2pitchPID = new() { P = 0.003, I = 0.00176, D = 0.00127 * 0.05, MinControl = -90.ToRad(), MaxControl = 90.ToRad() }; // oscillates at P=0.005 T=3.4s
-    private BasicPid _pitch2axisPID = new() { P = 4.2, I = 3.05, D = 1.44, MinControl = -0.3, MaxControl = 0.3 }; // oscillates at P=7 T=2.74s
+    private BasicPid _vspeed2pitchPID = new() { P = 0.003, I = 0.00176, D = 0.00127 * 0.05, MinControl = -90.ToRad(), MaxControl = 90.ToRad(), IntegrationLimit = 3 /*m/s / sec*/ }; // oscillates at P=0.005 T=3.4s
+    private BasicPid _pitch2axisPID = new() { P = 2, I = 1, D = 0.3, MinControl = -1, MaxControl = 1, IntegrationLimit = 1.ToRad() /*rad/sec*/ }; // oscillates at P=7 T=2.74s
     private SmoothMover _pitch = new(1, -1, 1);
 
     public double TargetAltitudeFt { get; set; } = 2000;
@@ -42,6 +43,7 @@ class BasicPid
     public double ErrorIntegral { get; set; }
     public double Derivative { get; set; }
     public double DerivativeSmoothing { get; set; } = 0.8;
+    public double IntegrationLimit { get; set; } // derivative must be less than this to start integrating error
     private double _prevError;
 
     public double Update(double error, double dt)
@@ -52,7 +54,7 @@ class BasicPid
         double output = P * error + I * ErrorIntegral + D * Derivative;
 
         output = output.Clip(MinControl, MaxControl);
-        if (output > MinControl && output < MaxControl)
+        if (Math.Abs(Derivative) < IntegrationLimit)
             ErrorIntegral += error * dt;
         return output;
     }
