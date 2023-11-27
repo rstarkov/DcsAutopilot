03785a81704074fd501eb696034465dd50718746
Roman Starkov <sourcecode@roman.st>
2023-11-27 17:16:25 +0000

    ParamPid

--- Src/Controllers.cs
+++ Src/Controllers.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using RT.Util.ExtensionMethods;
+﻿using RT.Util.ExtensionMethods;
 
 namespace DcsAutopilot;
 
@@ -18,6 +17,8 @@ public class BasicPid
     private double _prevError = double.NaN;
     public bool Integrating { get; private set; }
     public double Bias { get; set; }
+
+    // TODO: separate Min/Max limits for the P term of the output
 
     public double Update(double error, double dt)
     {
@@ -67,5 +68,126 @@ public class BasicPid
         I = 0.40 * pCritical / tOsc;
         D = 0.066 * pCritical * tOsc;
         return this;
+    }
+}
+
+public class ParamPidPt
+{
+    public string Name { get; set; }
+    public BasicPid Pid { get; set; }
+    public Vec Params { get; set; }
+    public double Scale { get; set; }
+
+    public ParamPidPt(BasicPid pid, params double[] vec)
+    {
+        Name = vec.JoinString(", ");
+        Pid = pid;
+        Params = new Vec(vec);
+    }
+}
+
+public class ParamPid
+{
+    public BasicPid PID { get; } = new();
+    public List<ParamPidPt> Points;
+
+    private int _dim;
+    private List<Line> _lines = new();
+    private Vec _min, _max;
+
+    public ParamPid(Vec min, Vec max, params ParamPidPt[] points) : this(min, max, points.AsEnumerable()) { }
+    public ParamPid(Vec min, Vec max, IEnumerable<ParamPidPt> points)
+    {
+        _min = min;
+        _max = max;
+        Points = points.ToList();
+        _dim = Points[0].Params.Count;
+        foreach (var pt in Points)
+            if (pt.Params.Count != _dim) throw new Exception($"All PIDs must be parameterised to the same number of dimensions.");
+        // scale per min/max
+        foreach (var pt in Points)
+            for (int i = 0; i < pt.Params.Count; i++)
+                pt.Params[i] = (pt.Params[i] - min[i]) / (max[i] - min[i]);
+        // pre-compute every possible line
+        if (Points.Count > 1)
+            foreach (var pts in Points.Subsequences(2, 2))
+                _lines.Add(new Line(pts.First(), pts.Last()));
+        // remove every line that has a point too close to the line and within the segment - as it will also exist as two separate lines, guaranteed
+        _lines.RemoveAll(line => Points.Any(pt =>
+        {
+            var t = line.ProjT(pt.Params);
+            var dist = line.Distance(pt.Params, t);
+            return t > 0 && t < 1 && dist < 0.01;
+        }));
+    }
+
+    public void Update(Vec state)
+    {
+        for (int i = 0; i < state.Count; i++)
+            state[i] = (state[i] - _min[i]) / (_max[i] - _min[i]);
+        foreach (var pt in Points)
+            pt.Scale = 0;
+        foreach (var line in _lines)
+        {
+            var t = line.ProjT(state);
+            var dist = line.Distance(state, t);
+            var linescale = 1 / dist;
+            var pt1scale = t < 0 ? 1 : t > 1 ? 0 : (1 - t);
+            var pt2scale = t < 0 ? 0 : t > 1 ? 1 : t;
+            line.Point1.Scale += linescale * pt1scale;
+            line.Point2.Scale += linescale * pt2scale;
+        }
+        if (Points.Count == 1)
+            Points[0].Scale = 1;
+        double totalscale = 0;
+        foreach (var pt in Points)
+            totalscale += pt.Scale;
+        PID.P = 0;
+        PID.I = 0;
+        PID.D = 0;
+        PID.Bias = 0;
+        PID.DerivativeSmoothing = 0;
+        PID.IntegrationLimit = 0;
+        PID.MinControl = 0;
+        PID.MaxControl = 0;
+        foreach (var pt in Points)
+        {
+            pt.Scale /= totalscale;
+            PID.P += pt.Scale * pt.Pid.P;
+            PID.I += pt.Scale * pt.Pid.I;
+            PID.D += pt.Scale * pt.Pid.D;
+            PID.Bias += pt.Scale * pt.Pid.Bias;
+            PID.DerivativeSmoothing += pt.Scale * pt.Pid.DerivativeSmoothing;
+            PID.IntegrationLimit += pt.Scale * pt.Pid.IntegrationLimit;
+            PID.MinControl += pt.Scale * pt.Pid.MinControl;
+            PID.MaxControl += pt.Scale * pt.Pid.MaxControl;
+        }
+    }
+
+    private class Line
+    {
+        public ParamPidPt Point1, Point2;
+        public Vec Dir;
+        private double _1overDirDotDir;
+
+        public Line(ParamPidPt pt1, ParamPidPt pt2)
+        {
+            Point1 = pt1;
+            Point2 = pt2;
+            Dir = Point2.Params - Point1.Params;
+            _1overDirDotDir = 1 / (Dir * Dir);
+        }
+
+        public double ProjT(Vec point) => (point - Point1.Params) * Dir * _1overDirDotDir;
+
+        public double Distance(Vec point, double projT)
+        {
+            if (projT <= 0)
+                return (point - Point1.Params).Abs();
+            else if (projT >= 1)
+                return (point - Point2.Params).Abs();
+            else
+                return (point - (Point1.Params + projT * Dir)).Abs();
+        }
     }
 }
---
+++ Src/Vec.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using RT.Util.ExtensionMethods;
+
+namespace DcsAutopilot;
+
+public class Vec : IList<double>
+{
+    private double[] _x;
+
+    public double this[int index] { get => _x[index]; set { _x[index] = value; } }
+    public int Count => _x.Length;
+
+    public bool IsReadOnly => false;
+    public bool Contains(double item) => _x.Contains(item);
+    public int IndexOf(double item) => _x.IndexOf(item);
+    public IEnumerator<double> GetEnumerator() => ((IEnumerable<double>)_x).GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => _x.GetEnumerator();
+    public void CopyTo(double[] array, int arrayIndex) => _x.CopyTo(array, arrayIndex);
+
+    public void Clear() { throw new NotSupportedException(); }
+    public void Add(double item) { throw new NotSupportedException(); }
+    public void Insert(int index, double item) { throw new NotSupportedException(); }
+    public bool Remove(double item) { throw new NotSupportedException(); }
+    public void RemoveAt(int index) { throw new NotSupportedException(); }
+
+    public Vec(IEnumerable<double> v) { _x = v.ToArray(); }
+    public Vec(params double[] v) { _x = v.ToArray(); }
+
+    public static Vec operator *(Vec v, double s) => new(v._x.Select(x => x * s));
+    public static Vec operator *(double s, Vec v) => new(v._x.Select(x => x * s));
+    public static Vec operator /(Vec v, double s) => new(v._x.Select(x => x / s));
+    public static Vec operator -(Vec v) => new(v._x.Select(x => -x));
+    public static Vec operator +(Vec v1, Vec v2)
+    {
+        sameLen(v1, v2);
+        var vr = new double[v1.Count];
+        for (int i = 0; i < v1.Count; i++)
+            vr[i] = v1[i] + v2[i];
+        return new Vec(vr);
+    }
+    public static Vec operator -(Vec v1, Vec v2)
+    {
+        sameLen(v1, v2);
+        var vr = new double[v1.Count];
+        for (int i = 0; i < v1.Count; i++)
+            vr[i] = v1[i] - v2[i];
+        return new Vec(vr);
+    }
+    public static double operator *(Vec v1, Vec v2)
+    {
+        sameLen(v1, v2);
+        double sum = 0;
+        for (int i = 0; i < v1.Count; i++)
+            sum += v1[i] * v2[i];
+        return sum;
+    }
+
+    private static void sameLen(Vec v1, Vec v2)
+    {
+        if (v1.Count != v2.Count)
+            throw new InvalidOperationException("This operation requires the vectors to be the same length");
+    }
+
+    public double Abs() => Math.Sqrt(_x.Sum(x => x * x));
+}
