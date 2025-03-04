e8ef4ff2781f02c6cc77f877a94f7b42645eb10a
Roman Starkov <sourcecode@roman.st>
2025-03-04 00:04:07 +0000

    Use global Dcs in MainWindow

--- Src/MainWindow.xaml.cs
+++ Src/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-using System.Text;
+﻿using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -12,7 +12,6 @@ namespace DcsAutopilot;
 
 public partial class MainWindow : ManagedWindow
 {
-    private DcsController _dcs;
     private DispatcherTimer _refreshTimer = new();
     private DispatcherTimer _sliderTimer = new();
     private SmoothMover _sliderMover = new(10.0, -1, 1);
@@ -24,8 +23,7 @@ public partial class MainWindow : ManagedWindow
     {
         InitializeComponent();
         Dcs = new();
-        _dcs = Dcs;
-        _dcs.LoadConfig();
+        Dcs.LoadConfig();
         _refreshTimer.Interval = TimeSpan.FromMilliseconds(100);
         _refreshTimer.Tick += refreshTimer_Tick;
         _sliderTimer.Interval = TimeSpan.FromMilliseconds(10);
@@ -39,13 +37,13 @@ public partial class MainWindow : ManagedWindow
         LineY.Pen = new Pen(Brushes.Yellow, 1);
         foreach (var line in ctChart.Lines)
             line.Pen.Freeze();
-        _dcs.FlightControllers.Clear();
-        _dcs.FlightControllers.Add(new ChartPopulate(this));
-        _dcs.FlightControllers.Add(_ctrl = new RollAutoTrim());
-        _dcs.FlightControllers.Add(new SmartThrottle());
-        foreach (var c in _dcs.FlightControllers)
-            c.Dcs = _dcs;
-        ctControllers.ItemsSource = _dcs.FlightControllers;
+        Dcs.FlightControllers.Clear();
+        Dcs.FlightControllers.Add(new ChartPopulate(this));
+        Dcs.FlightControllers.Add(_ctrl = new RollAutoTrim());
+        Dcs.FlightControllers.Add(new SmartThrottle());
+        foreach (var c in Dcs.FlightControllers)
+            c.Dcs = Dcs;
+        ctControllers.ItemsSource = Dcs.FlightControllers;
 
         btnStop_Click(null, null);
     }
@@ -68,48 +66,48 @@ public partial class MainWindow : ManagedWindow
         uiSmartThrottle.UpdateGuiTimer();
         uiRollAutoTrim.UpdateGuiTimer();
 
-        var status = _dcs.Status;
-        if (status == "Active control" && (DateTime.UtcNow - _dcs.LastFrameUtc).TotalMilliseconds > 250)
+        var status = Dcs.Status;
+        if (status == "Active control" && (DateTime.UtcNow - Dcs.LastFrameUtc).TotalMilliseconds > 250)
             status = $"Stalled; waiting for DCS";
-        if (_dcs.Warnings.Count > 0)
-            status = $"{_dcs.Warnings.Count} warnings: {_dcs.Warnings.First()} ...";
+        if (Dcs.Warnings.Count > 0)
+            status = $"{Dcs.Warnings.Count} warnings: {Dcs.Warnings.First()} ...";
         lblStatus.Content = status;
 
-        var fpsnew = (DateTime.UtcNow, _dcs.Frames);
+        var fpsnew = (DateTime.UtcNow, Dcs.Frames);
         _fps.Enqueue(fpsnew);
         while (_fps.Count > 0 && _fps.Peek().ts < DateTime.UtcNow.AddSeconds(-1))
             _fps.Dequeue();
         var fps = _fps.Count > 1 ? (fpsnew.Item2 - _fps.Peek().frames) / (fpsnew.Item1 - _fps.Peek().ts).TotalSeconds : 0;
-        var latency = _dcs.Latencies.Count() > 0 ? _dcs.Latencies.Average() : 0;
-        var statsStr = $"FPS: {fps:0}   Skips: {_dcs.Skips:#,0} of {_dcs.Frames:#,0}   Bytes: {_dcs.LastFrameBytes:#,0}";
+        var latency = Dcs.Latencies.Count() > 0 ? Dcs.Latencies.Average() : 0;
+        var statsStr = $"FPS: {fps:0}   Skips: {Dcs.Skips:#,0} of {Dcs.Frames:#,0}   Bytes: {Dcs.LastFrameBytes:#,0}";
         if (latency > 0)
             statsStr = $"Latency: {latency * 1000:0.0}ms   " + statsStr;
         lblStats.Content = statsStr;
 
         var sb = new StringBuilder();
-        sb.AppendLine($"Altitude ASL: {_dcs.LastFrame?.AltitudeAsl.MetersToFeet():#,0.000} ft");
-        sb.AppendLine($"Vertical speed: {_dcs.LastFrame?.SpeedVertical.MetersToFeet() * 60:#,0.000} ft/min");
-        sb.AppendLine($"AoA: {_dcs.LastFrame?.AngleOfAttack:0.00}°    AoSS: {_dcs.LastFrame?.AngleOfSideSlip:0.000}°");
-        sb.AppendLine($"Mach: {_dcs.LastFrame?.SpeedMach:0.00000}    IAS: {_dcs.LastFrame?.SpeedIndicated.MsToKts():0.0} kts");
-        sb.AppendLine($"Pitch: {_dcs.LastFrame?.Pitch:0.00}°/{_dcs.LastFrame?.VelPitch:0.00}°   Bank: {_dcs.LastFrame?.Bank:0.00}°   Hdg: {_dcs.LastFrame?.Heading:0.00}°");
-        sb.AppendLine($"Gyros: pitch={_dcs.LastFrame?.GyroPitch:0.00}   roll={_dcs.LastFrame?.GyroRoll:0.00}   yaw={_dcs.LastFrame?.GyroYaw:0.00}");
-        sb.AppendLine($"Joystick: {_dcs.LastFrame?.JoyPitch:0.000}   {_dcs.LastFrame?.JoyRoll:0.000}   {_dcs.LastFrame?.JoyYaw:0.000}   T:{_dcs.LastFrame?.JoyThrottle1:0.000}");
-        sb.AppendLine($"Flaps: {_dcs.LastFrame?.Flaps:0.000}   Speedbrakes: {_dcs.LastFrame?.Airbrakes:0.000}   Gear: {_dcs.LastFrame?.LandingGear:0.000}");
-        sb.AppendLine($"Acc: {_dcs.LastFrame?.AccX:0.000} / {_dcs.LastFrame?.AccY:0.000} / {_dcs.LastFrame?.AccZ:0.000}");
-        sb.AppendLine($"Test: {_dcs.LastFrame?.FuelFlow:#,0} / {_dcs.LastFrame?.Test1:0.000} / {_dcs.LastFrame?.Test2:0.000} / {_dcs.LastFrame?.Test3:0.000} / {_dcs.LastFrame?.Test4:0.000}");
+        sb.AppendLine($"Altitude ASL: {Dcs.LastFrame?.AltitudeAsl.MetersToFeet():#,0.000} ft");
+        sb.AppendLine($"Vertical speed: {Dcs.LastFrame?.SpeedVertical.MetersToFeet() * 60:#,0.000} ft/min");
+        sb.AppendLine($"AoA: {Dcs.LastFrame?.AngleOfAttack:0.00}°    AoSS: {Dcs.LastFrame?.AngleOfSideSlip:0.000}°");
+        sb.AppendLine($"Mach: {Dcs.LastFrame?.SpeedMach:0.00000}    IAS: {Dcs.LastFrame?.SpeedIndicated.MsToKts():0.0} kts");
+        sb.AppendLine($"Pitch: {Dcs.LastFrame?.Pitch:0.00}°/{Dcs.LastFrame?.VelPitch:0.00}°   Bank: {Dcs.LastFrame?.Bank:0.00}°   Hdg: {Dcs.LastFrame?.Heading:0.00}°");
+        sb.AppendLine($"Gyros: pitch={Dcs.LastFrame?.GyroPitch:0.00}   roll={Dcs.LastFrame?.GyroRoll:0.00}   yaw={Dcs.LastFrame?.GyroYaw:0.00}");
+        sb.AppendLine($"Joystick: {Dcs.LastFrame?.JoyPitch:0.000}   {Dcs.LastFrame?.JoyRoll:0.000}   {Dcs.LastFrame?.JoyYaw:0.000}   T:{Dcs.LastFrame?.JoyThrottle1:0.000}");
+        sb.AppendLine($"Flaps: {Dcs.LastFrame?.Flaps:0.000}   Speedbrakes: {Dcs.LastFrame?.Airbrakes:0.000}   Gear: {Dcs.LastFrame?.LandingGear:0.000}");
+        sb.AppendLine($"Acc: {Dcs.LastFrame?.AccX:0.000} / {Dcs.LastFrame?.AccY:0.000} / {Dcs.LastFrame?.AccZ:0.000}");
+        sb.AppendLine($"Test: {Dcs.LastFrame?.FuelFlow:#,0} / {Dcs.LastFrame?.Test1:0.000} / {Dcs.LastFrame?.Test2:0.000} / {Dcs.LastFrame?.Test3:0.000} / {Dcs.LastFrame?.Test4:0.000}");
         sb.AppendLine("Controller: " + _ctrl?.Status);
         sb.AppendLine();
         lblInfo.Text = sb.ToString();
 
         void setSlider(Slider sl, double? value)
         {
-            sl.IsEnabled = _dcs.IsRunning ? value != null : false;
-            sl.Value = _dcs.IsRunning ? value ?? 0 : 0;
+            sl.IsEnabled = Dcs.IsRunning ? value != null : false;
+            sl.Value = Dcs.IsRunning ? value ?? 0 : 0;
         }
-        setSlider(ctrlPitch, -_dcs.LastControl?.PitchAxis);
-        setSlider(ctrlRoll, _dcs.LastControl?.RollAxis);
-        setSlider(ctrlYaw, _dcs.LastControl?.YawAxis);
-        setSlider(ctrlThrottle, _dcs.LastControl?.ThrottleAxis);
+        setSlider(ctrlPitch, -Dcs.LastControl?.PitchAxis);
+        setSlider(ctrlRoll, Dcs.LastControl?.RollAxis);
+        setSlider(ctrlYaw, Dcs.LastControl?.YawAxis);
+        setSlider(ctrlThrottle, Dcs.LastControl?.ThrottleAxis);
 
         ctChart.InvalidateVisual();
 
@@ -123,12 +121,12 @@ public partial class MainWindow : ManagedWindow
         }
         lblChartInfo.Text = $"Oscillation:  R={oscPeriod(LineR)}  G={oscPeriod(LineG)}  Y={oscPeriod(LineY)}";
 
-        if (_dcs.LastFrame != null)
+        if (Dcs.LastFrame != null)
         {
-            var windabs = new PointD(_dcs.LastFrame.WindX.MsToKts(), _dcs.LastFrame.WindZ.MsToKts());
-            var dir = new PointD(_dcs.LastFrame.Heading.ToRad());
+            var windabs = new PointD(Dcs.LastFrame.WindX.MsToKts(), Dcs.LastFrame.WindZ.MsToKts());
+            var dir = new PointD(Dcs.LastFrame.Heading.ToRad());
             ctWindComp.SetWind(-windabs.Dot(dir), -windabs.Dot(dir.Rotated(Math.PI / 2)));
-            ctWindDir.SetWind(windabs.Theta().ToDeg(), windabs.Abs(), _dcs.LastFrame.Heading);
+            ctWindDir.SetWind(windabs.Theta().ToDeg(), windabs.Abs(), Dcs.LastFrame.Heading);
         }
     }
 
@@ -152,7 +150,7 @@ public partial class MainWindow : ManagedWindow
                 LineY.Data.Enqueue(Math.Atan2(frame.VelY, Math.Sqrt(frame.VelX * frame.VelX + frame.VelZ * frame.VelZ)));
                 //LineY.Data.Enqueue(frame.AccY);
                 //LineY.Data.Enqueue(frame.Pitch);
-                LineR.Data.Enqueue(_wnd._dcs.LastControl?.PitchAxis ?? 0);
+                LineR.Data.Enqueue(Dcs.LastControl?.PitchAxis ?? 0);
                 //LineR.Data.Enqueue(frame.Bank);
             }
             _skip++;
@@ -166,14 +164,14 @@ public partial class MainWindow : ManagedWindow
         refreshTimer_Tick(sender, null);
         foreach (var line in ctChart.Lines)
             line.Data.Clear();
-        _dcs.Start();
+        Dcs.Start();
         UpdateGui();
     }
 
     private void btnStop_Click(object sender, RoutedEventArgs e)
     {
-        _dcs.Stop();
-        _dcs.SaveConfig();
+        Dcs.Stop();
+        Dcs.SaveConfig();
         _refreshTimer.Stop();
         refreshTimer_Tick(sender, null);
         lblStats.Content = "";
@@ -186,9 +184,9 @@ public partial class MainWindow : ManagedWindow
         if (_bobblehead != null)
             return;
         _bobblehead = new BobbleheadWindow();
-        if (!_dcs.FlightControllers.OfType<BobbleheadController>().Any())
-            _dcs.FlightControllers.Add(new BobbleheadController());
-        var ctrl = _dcs.FlightControllers.OfType<BobbleheadController>().Single();
+        if (!Dcs.FlightControllers.OfType<BobbleheadController>().Any())
+            Dcs.FlightControllers.Add(new BobbleheadController());
+        var ctrl = Dcs.FlightControllers.OfType<BobbleheadController>().Single();
         _bobblehead.Closing += delegate { _bobblehead = null; ctrl.Window = null; };
         _bobblehead.Show();
         ctrl.Window = _bobblehead;
@@ -204,8 +202,8 @@ public partial class MainWindow : ManagedWindow
 
     private void UpdateGui()
     {
-        btnStart.IsEnabled = !_dcs.IsRunning;
-        btnStop.IsEnabled = _dcs.IsRunning;
+        btnStart.IsEnabled = !Dcs.IsRunning;
+        btnStop.IsEnabled = Dcs.IsRunning;
         uiSmartThrottle.UpdateGui();
         uiRollAutoTrim.UpdateGui();
         refreshTimer_Tick(null, null);
