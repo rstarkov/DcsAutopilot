b81705def564abe04b2b3f7335bb5a2c20cd2fb1
Roman Starkov <sourcecode@roman.st>
2025-04-04 23:23:29 +0100

    CsvDump helper

---
+++ Src/Core.Util/CsvDump.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Concurrent;
+using System.IO;
+
+namespace DcsAutopilot;
+
+public static class CsvDump
+{
+    public static ConcurrentQueue<string> Lines = new();
+
+    public static void AddLine(string line)
+    {
+        Lines.Enqueue(line);
+        if (Lines.Count < 100)
+            return;
+        Task.Run(() =>
+        {
+            var lines = new List<string>();
+            lock (Lines) // only from other instances of this same task to avoid interleaved dequeue or file conflicts
+            {
+                while (Lines.TryDequeue(out var s))
+                    lines.Add(s);
+                File.AppendAllLines("dump.csv", lines);
+            }
+        });
+    }
+}
